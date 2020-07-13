99e5f16fdcc09e40d01211bebb60cfe47203994a
azick7553 <azick7553@gmail.com>
2020-07-13 18:22:44 +0500

    Prepare cors and remove https.

--- API/Startup.cs
+++ API/Startup.cs
@@ -26,6 +26,13 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
